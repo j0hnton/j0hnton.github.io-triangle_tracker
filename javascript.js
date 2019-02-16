6ad4d40b889644dd23efe542ffeb263d8606ae17
j0hnton <afrikiddo@gmail.com>
2019-02-16 20:42:38 +0300

    Further syling

--- javascript.js
+++ javascript.js
@@ -1,23 +1,39 @@
-function track() {
-  var a =parseInt(document.getElementById('a').value);
-  var b =parseInt(document.getElementById('b').value);
-  var c =parseInt(document.getElementById('c').value);
+function  tracker(){
+  /*USER INTERFACE LOGIC*/
 
-  if (a+b>c&& b+c>a && a+c>b){
-    if (a==b && b==c && a==c) {
-      alert("This is an equillateral triangle")
-    }
-    else if (a!==b && b!=a && a!==c){
-    alert("THIS IS A SCALENE TRIANGLE")
-    }
-    else{
-      alert("THE TRIANGLE IS ISOSCELESS")
-    }
+  var collect1=document.getElementById("sideA").value;
+  var collect2=document.getElementById("sideB").value;
+  var collect3=document.getElementById("sideC").value;
+  var sidea=parseInt(collect1);
+  var sideb=parseInt(collect2);
+  var sidec=parseInt(collect3);
+  var sides=[sidea,sideb,sidec];
+  var type=["makes an equilateral triangle","makes an isosceles triangle","makes an scalene triangle","doesn't make a triangle"]
+
+  /*BUSINESS LOGIC*/
+
+  if(sides[0] <=0 || sides[1] <=0 || sides[2] <=0){
+    var firstOutput=sides[0]+" "+sides[1]+" "+sides[2]+" "+type[3];
+    document.getElementById("trackOutput").innerHTML=firstOutput;
   }
-  else{
-    alert("THIS IS NOT A TRIANGLE")
+  else if(sides[0]===sides[1] && sides[1]===sides[2] && sides[2]===sides[0]){
+    var secondOutput=sides[0]+" "+sides[1]+" "+sides[2]+" "+type[0];
+    document.getElementById("trackOutput").innerHTML=secondOutput;
   }
-  p{
-    color:white;
-    font-size:200;
+  else if(sides[0]===sides[1] || sides[0]===sides[2] || sides[1]===sides[2]){
+    var thirdOutput=sides[0]+" "+sides[1]+" "+sides[2]+" "+type[1];
+    document.getElementById("trackOutput").innerHTML=thirdOutput;
   }
+  else if(sides[0]!==sides[1] && sides[0]!==sides[2] && sides[2]!==sides[1] && sides[0]+sides[1]>sides[2] && sides[1]+sides[2]>sides[0] && sides[2]+sides[0]>sides[1]){
+    var fourthOutput=sides[0]+" "+sides[1]+" "+sides[2]+" "+type[2];
+    document.getElementById("trackOutput").innerHTML=fourthOutput;
+  }
+  else if(sides[0]+sides[1]<sides[2] || sides[1]+sides[2]<sides[0] && sides[2]+sides[0]<sides[1]){
+    var fifthOutput=sides[0]+" "+sides[1]+" "+sides[2]+" "+type[3];
+    document.getElementById("trackOutput").innerHTML=fifthOutput;
+  }
+  else {
+    var sixthOutput=type[3];
+    document.getElementById("trackOutput").innerHTML=sixthOutput;
+  }
+}
