1eb70966459fb16392a87e89fc2835a4fc12f3a9
j0hnton <afrikiddo@gmail.com>
2019-02-18 10:47:19 +0300

    Debugging

---
+++ javascript.js
@@ -0,0 +1,29 @@
+function  tracker(){
+
+  var lengthA=parseInt(document.getElementById("a").value);
+  var lengthB=parseInt(document.getElementById("b").value);
+  var lengthC=parseInt(document.getElementById("c").value);
+  var triangle=[lengthA,lengthB,lengthC];
+
+ if(triangle[0]===triangle[1] && triangle[1]===triangle[2]  && triangle[0]===triangle[2]){
+
+    document.getElementById("result").innerHTML='EQUILATERAL';
+  }
+
+  else if(triangle[0]!==triangle[1] && triangle[0]!==triangle[1] && triangle [0]!==triangle[2] && triangle[2]!==triangle[0] && triangle[1]!==triangle[2] && triangle [2]!==triangle[1]){
+
+    document.getElementById("result").innerHTML='SCALENE';
+  }
+
+  else if (triangle[0]===triangle[1] || triangle[0]===triangle[2] || triangle[1]===triangle[2]){
+
+      document.getElementById("result").innerHTML='ISOSCELESS';
+    }
+
+  else if (triangle[0]+triangle[1]<triangle[2] || triangle[1]+triangle[2]<triangle[0] || triangle[2]+triangle[0]<triangle[1]){
+      document.getElementById("result").innerHTML="NOT A TRIANGLE";
+}
+else (triangle[0]+triangle[1]===triangle[2] || triangle[1]+triangle[2]===triangle[0] || triangle[2]+triangle[0]===triangle[1]);{
+document.getElementsByTagName("result").innerHTML="NOT A TRIANGLE";
+}
+}
