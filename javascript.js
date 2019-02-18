2dd7524551a32cec5eace68006ce0f9562126b89
j0hnton <afrikiddo@gmail.com>
2019-02-18 12:21:42 +0300

    More styling

--- javascript.js
+++ javascript.js
@@ -10,7 +10,11 @@ function  tracker(){
     document.getElementById("result").innerHTML='EQUILATERAL';
   }
 
-  else if(triangle[0]!==triangle[1] && triangle[0]!==triangle[1] && triangle [0]!==triangle[2] && triangle[2]!==triangle[0] && triangle[1]!==triangle[2] && triangle [2]!==triangle[1]){
+  else if(triangle[0]!= triangle[1] && triangle [0]!= triangle[2] &&  triangle[1]!= triangle[2]){
+
+    document.getElementById("result").innerHTML='SCALENE';
+  }
+  else if(triangle[0]+triangle[1]>triangle[2] || triangle[0]+triangle[2]>triangle[1] || triangle[2]+triangle[1]>triangle [0]){
 
     document.getElementById("result").innerHTML='SCALENE';
   }
@@ -20,7 +24,8 @@ function  tracker(){
       document.getElementById("result").innerHTML='ISOSCELESS';
     }
 
-  else if (triangle[0]+triangle[1]<triangle[2] || triangle[1]+triangle[2]<triangle[0] || triangle[2]+triangle[0]<triangle[1] && triangle[0]+triangle[1]!==triangle[2] || triangle[1]+triangle[2]!==triangle[0] || triangle[2]+triangle[0]!==triangle[1] ){
+  else if ((lengthA+lengthB!=lengthC) || (lengthA+lengthC!=lengthB) || (lengthB+lengthC!=lengthA) && ((lengthA+lengthB<lengthC) || (lengthB+lengthC<lengthA)||(lengthA+lengthC<lengthB))){
+console.log("working")
       document.getElementById("result").innerHTML="NOT A TRIANGLE";
 }
 }
